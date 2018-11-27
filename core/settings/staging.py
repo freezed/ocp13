5e7e5efb683d5bbb35c4469fc7715a752afd7c6c
Fred Z <freezed@users.noreply.github.com>
2018-11-27 18:05:09 +0100

    Set `sentry-sdk` in staging ENV FIX #3

--- core/settings/staging.py
+++ core/settings/staging.py
@@ -2,9 +2,18 @@
 `settings.py` for staging environement
 Django settings for ocp13 project.
 """
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from . import *
 
 DEBUG = False
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['ocp13-1664.herokuapp.com']
+
+sentry_sdk.init(
+    dsn="https://9f531f2ee4354cd09dcbd64066283928@sentry.io/1331802",
+    integrations=[DjangoIntegration()],
+    environment="staging",
+)
