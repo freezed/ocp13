c9a18dbcf5d9888bbe6ac0f146c05f7e50c0d020
Fred Z <freezed@users.noreply.github.com>
2018-11-27 14:36:07 +0100

    Create app organact FIX #1

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'organact.apps.OrganactConfig',
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from organact import views as organact_views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
+    path('', organact_views.home, name='home'),
 ]
