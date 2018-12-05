2eec2c2a03d9bb7887d7b077298177670eb44ca1
Fred Z <freezed@users.noreply.github.com>
2018-12-05 17:11:00 +0100

    Add 1st HTML templates #7

--- organact/views.py
+++ organact/views.py
@@ -1,4 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home(request):
-    return HttpResponse("Hello, world. Here's the `ocp13` homepage.")
+    return render(request, 'home.html')
