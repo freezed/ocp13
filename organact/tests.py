834847754cffd765bcf18c4d30d49b35a9a6c456
Fred Z <freezed@users.noreply.github.com>
2018-12-05 16:35:18 +0100

    Add test for using HTML template #7

--- organact/tests.py
+++ organact/tests.py
@@ -1,15 +1,29 @@
-import pytest
+from pytest import fixture
 
 from django.test import Client
+
+@fixture
+def templates():
+    """ set basic template """
+    templates = ['base.html']
+
+    return templates
 
 ################################################################################
 #   organact.views.home()
 ################################################################################
-def test_home():
+def test_home(templates):
+    templates.insert(0, 'home.html')
     CLIENT = Client()
     response = CLIENT.get('/')
 
     assert response.status_code == 200
+    assert templates == [t.name for t in response.templates]
+
+
+################################################################################
+#   organact.views - error
+################################################################################
 
 def test_404():
     CLIENT = Client()
