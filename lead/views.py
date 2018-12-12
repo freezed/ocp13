13afd8f9e669fc3520b049c293b533c665624331
Fred Z <freezed@users.noreply.github.com>
2018-12-12 23:51:08 +0100

    Format template lead.index #10

--- lead/views.py
+++ lead/views.py
@@ -12,17 +12,28 @@ def index(request):
     if request.user.is_anonymous:
         return redirect('login')
 
+    label_list = [
+        'first_name',
+        'last_name',
+        'phone',
+        'email',
+        'organization',
+        'date_joined',
+        'date_edit',
+    ]
+
     context = {
         'contacts': 'Pas de contacts',
         'status': False
     }
 
-    contact_dict = Contact.objects.values().filter(user=request.user)
+    contact_dict = Contact.objects.values(*label_list).filter(user=request.user)
 
     if contact_dict.count() > 0:
         context.update({
             'contacts': contact_dict,
             'status': True,
+            'label_list': label_list,
         })
 
     return render(request, 'lead/index.html', context)
