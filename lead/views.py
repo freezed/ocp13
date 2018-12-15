ed7075497fdf31e95e7d157c637e692173275353
Fred Z <freezed@users.noreply.github.com>
2018-12-15 19:40:48 +0100

    Use Class Based View for a contact page #10

--- lead/views.py
+++ lead/views.py
@@ -1,14 +1,30 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404, render, redirect
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from lead.models import Contact
 
 
 class ContactList(LoginRequiredMixin, ListView):
+    """
+    Index page of `lead` app, show contact list of the authenticated user
+    """
     model = Contact
     context_object_name = 'contact_list'
+
+
+class ContactDetail(LoginRequiredMixin, DetailView):
+    """
+    Show details of the choosen contact
+    """
+    model = Contact
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['contact_fields'] = Contact.objects.all()
+
+        return context
 
 
 def edit(request, contact_id):
@@ -19,16 +35,6 @@ def edit(request, contact_id):
     contact = get_object_or_404(Contact, id=contact_id, user=request.user)
 
     return render(request, "lead/edit.html", {'contact': contact})
-
-
-def view(request, contact_id):
-
-    if request.user.is_anonymous:
-        return redirect('login')
-
-    contact = get_object_or_404(Contact, id=contact_id, user=request.user)
-
-    return render(request, "lead/view.html", {'contact': contact})
 
 
 def add(request):
