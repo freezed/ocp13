c6823a721195dc85a0b338098888543dddaf1f0c
Fred Z <freezed@users.noreply.github.com>
2018-12-15 21:53:16 +0100

    Add a `pk_url_kwarg` to `ContactDetail` #10

--- lead/urls.py
+++ lead/urls.py
@@ -6,6 +6,6 @@ app_name = 'lead'
 urlpatterns = [
     path('', lead_views.ContactList.as_view(), name='index'),
     path('add/', lead_views.add, name='add'),
-    path('<int:pk>', lead_views.ContactDetail.as_view(), name='view'),
+    path('<int:contact_id>', lead_views.ContactDetail.as_view(), name='view'),
     path('<int:contact_id>/edit/', lead_views.edit, name='edit'),
 ]
